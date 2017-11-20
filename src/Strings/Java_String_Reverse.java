236a7145575a26729317c66e3eaa1ac01a347d79
Ishan <ishanagarwal.cs@gmail.com>
2017-11-19 23:06:28 -0500

    Hacker Rank JAVA Commit: 7

--- src/Strings/Java_String_Reverse.java
+++ src/Strings/Java_String_Reverse.java
@@ -4,20 +4,16 @@ import java.util.Scanner;
 
 public class Java_String_Reverse {
 
-	public static void main(String[] args) {
-
-		Scanner sc=new Scanner(System.in);
-		String A=sc.next();
-		/* Enter your code here. Print output to STDOUT. */
-		
-		String palin_A = new StringBuffer(A).reverse().toString();
-
-		if(A.equals(palin_A))
-			System.out.println("Yes");
-		else
-			System.out.println("No");
-
-		sc.close();
-
-	}
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+      
+    
+      System.out.println(A.equals(new StringBuilder(A).reverse().toString())? "Yes":"No");
+      sc.close();
+    
+        
+    }
 }
